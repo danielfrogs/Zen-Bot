375d4c812af76d735157c7fb25811c6cf89adef9
Daniel Behan <danielfrogs@users.noreply.github.com>
2017-04-15 01:09:33 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
-const commando = require("discord.js-commando");
-const bot = new commando.Client();
+const Discord = require("discord.js");
+const bot = new Discord.Client({autoReconnect: true, maxMessageCache: 0});
 
-//This allows for commands to be located in their own .js file instead of all being located in one file
-bot.registry.registerGroup("public", "Admin");
-bot.registry.registerDefaults();
-bot.registry.registerCommandsIn(__dirname + "/commands");
+//The bot token goes within the quote marks
+const token = "BOT TOKEN GOES HERE";
 
-//The bot token goes within the quote marks and this will log in the Discord bot application
-bot.login("BOT TOKEN GOES HERE");
+//The bot will only start reacting to information after ready is outputted
+bot.on("ready", () => {
+    console.log("Bot is active!");
+});
+
+//Event listener for when the message "ping" is inputted
+bot.on("message", message => {
+    if(message.content === "ping") {
+        message.channel.sendMessage("pong");
+    }
+});
+
+//Event listener for when both "joinvoice" and "leavevoice" are inputted
+bot.on("message", message => {
+    //Voice channel ID that the bot will connect to when the command "joinvoice" is inputted goes within the quote marks
+    let voiceChannel = bot.channels.get("296737276405350402");
+    if(message.content === "joinvoice") {
+        voiceChannel.join()
+        .then(connection => {
+            //Audio file directory, in this case the root directory goes within the quote marks
+            return connection.playFile("./song.wav");
+        })
+        .then(dispatcher => {
+            dispatcher.on("error", console.error);
+        })
+        .catch(console.error);
+    }
+    if(message.content === "leavevoice") {
+        voiceChannel.leave();
+    }
+});
+
+//Log the Discord bot application in
+bot.login(token);
+
+
