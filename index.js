d4bfbd405e95c07b83833356074631c14649563e
Daniel Behan <danielfrogs@users.noreply.github.com>
2017-04-17 03:03:26 +0100

    Add files via upload
    - added 2 arrays (1 used for YouTube urls to play and second to specify the length in milliseconds each url is in the same order for both arrays)
    - loop plays tracks 1 by 1 awaiting previous to finish - may still be some bugs with timing

--- index.js
+++ index.js
@@ -17,19 +17,74 @@ bot.on("message", message => {
 });
 
 //Event listener for when both "joinvoice" and "leavevoice" are inputted
+//bot.on("message", message => {
+    //Voice channel ID that the bot will connect to when the command "joinvoice" is inputted goes within the quote marks
+    //let voiceChannel = bot.channels.get("296737276405350402");
+    //if(message.content === "joinvoice") {
+        //voiceChannel.join()
+        //.then(connection => {
+            //Audio file directory, in this case the root directory goes within the quote marks
+            //return connection.playFile("./song.wav");
+        //})
+        //.then(dispatcher => {
+            //dispatcher.on("error", console.error);
+        //})
+        //.catch(console.error);
+    //}
+    //if(message.content === "leavevoice") {
+        //voiceChannel.leave();
+    //}
+//});
+
+//var test = ["a", "b", "c"];
+
+//Array for the list of YouTube urls to pull when looping over voice streams
+let urlsPlaylist = [
+    "https://www.youtube.com/watch?v=JbjzPKTfjlc",
+    "https://www.youtube.com/watch?v=JbjzPKTfjlc",
+    "https://www.youtube.com/watch?v=no3B0uS6nLk",
+    "https://www.youtube.com/watch?v=3oQHMREyXSw"
+] 
+
+//Array for the total time to delay the loop in milliseconds dependent on the YouTube stream that is being played (must be in same order as urls are in urlPlaylist array)
+let urlsTimePlaylist = [
+    360000,
+    360000,
+    554000,
+    224000
+]
+
+var i = 1;
+
+//Event listener for when both "joinvoice" and "leavevoice" are inputted
 bot.on("message", message => {
     //Voice channel ID that the bot will connect to when the command "joinvoice" is inputted goes within the quote marks
-    let voiceChannel = bot.channels.get("296737276405350402");
+    let voiceChannel = bot.channels.get("VOICE CHANNEL ID GOES HERE");
+    const ytdl = require("ytdl-core");
+    const streamOptions = {seek: 0, volume: 1};
     if(message.content === "joinvoice") {
-        voiceChannel.join()
-        .then(connection => {
-            //Audio file directory, in this case the root directory goes within the quote marks
-            return connection.playFile("./song.wav");
-        })
-        .then(dispatcher => {
-            dispatcher.on("error", console.error);
-        })
-        .catch(console.error);
+        function myLoop() {
+            //Random number with the number at the end always being the total number urls in the urlsPlaylist array to give a number within the range of the total amount which will be used to select a track
+            let random = urlsPlaylist.length - Math.floor(Math.random() * 4); 
+                setInterval(function () {
+                    //Checks to see if loop is continuing
+                    console.log("Logged");
+                    i++;
+                    if(i < 10) {
+                        myLoop();
+                    }
+                }, urlsTimePlaylist[random]);
+                console.log("Selected placeholder:", random);
+                console.log("Time wiat in milliseconds:", urlsTimePlaylist[random]);
+                //Bot joins voice channel and plays the YouTube stream collected from urlsPlaylist array
+                voiceChannel.join()
+                .then(connection => {
+                    const stream = ytdl(urlsPlaylist[random], {filter: "audioonly", quality: "lowest"});
+                    return connection.playStream(stream, streamOptions);
+                })
+            .catch(console.error);
+        }    
+        myLoop()
     }
     if(message.content === "leavevoice") {
         voiceChannel.leave();
