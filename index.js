8bfb589a4614878f612e5b9f954d0eb4167286a9
Daniel Behan <danielfrogs@users.noreply.github.com>
2017-04-20 21:52:31 +0100

    Add files via upload
    - moved the urlsPlaylist array to a separate javascript file away from the index.js javascript file to save clutter. Thinking of adding a url requested song player which checks to see whether requested url is in the array, if not add it to there so the array of songs continuously grows along with role permissions.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client({autoReconnect: true, maxMessageCache: 0});
 
+const urlsPlaylist = require("./urlsArray.js").urlsPlaylist;
+
 //The bot token goes within the quote marks
 const token = "BOT TOKEN GOES HERE";
 
@@ -20,7 +22,7 @@ bot.on("message", message => {
 bot.on("message", message => {
     //Voice channel ID that the bot will connect to when the command "joinvoice" is inputted goes within the quote marks
     let voiceChannel = bot.channels.get("296737276405350402");
-    if(message.content === "play-track") {
+    if(message.content === "play track") {
         voiceChannel.join()
         .then(connection => {
             //Audio file directory, in this case the root directory goes within the quote marks
@@ -33,22 +35,13 @@ bot.on("message", message => {
     }
 });
 
-//Array for the list of YouTube urls to pull when looping over voice streams
-let urlsPlaylist = [
-    "https://www.youtube.com/watch?v=JbjzPKTfjlc", //Position 0
-    "https://www.youtube.com/watch?v=I2rmvWNwKz0",
-    "https://www.youtube.com/watch?v=no3B0uS6nLk",
-    "https://www.youtube.com/watch?v=3oQHMREyXSw",
-    "https://www.youtube.com/watch?v=L7I9we5jP3I"
-]
-
 //Event listener for when both "joinvoice" and "leavevoice" are inputted
 bot.on("message", message => {
     //Voice channel ID that the bot will connect to when the command "joinvoice" is inputted goes within the quote marks
     let voiceChannel = bot.channels.get("VOICE CHANNEL ID GOES HERE");
     const ytdl = require("ytdl-core");
-    const streamOptions = {seek: 0, volume: 1};
-    if(message.content === "play-youtube") {
+    const streamOptions = {seek: 0, volume: 0.5};
+    if(message.content === "play youtube") {
         function looping() {
             //Random number representing position within the urlsPlaylist array to select url. End number must be equal to the total amount of possibly selected urls from the array discounting position 0
             randomPosition = Math.round(Math.random() * 4); 
@@ -67,10 +60,22 @@ bot.on("message", message => {
         }
         looping();
     }
-    if(message.content === "leavevoice") {
-    voiceChannel.leave();
-    }
 });
+
+    //if(message.content.startsWith("https://www.youtube.com/watch?v=")) {
+        //voiceChannel.join()
+        //.then(connection => {
+            //let stream = ytdl(message.content, {filter: "audioonly", quality: "lowest"});
+            //const dispatcher = connection.playStream(stream, streamOptions);
+            //setInterval(connection.speaking)
+            //dispatcher.on("end", () => {
+                //voiceChannel.leave();
+            //});
+        //});
+    //}
+//});
+
+
 
 //Log the Discord bot application in
 bot.login(token);
---
+++ urlsArray.js
@@ -0,0 +1,8 @@
+//Array for the list of YouTube urls to pull when looping over voice streams
+exports.urlsPlaylist = [
+"https://www.youtube.com/watch?v=JbjzPKTfjlc", //Position 0
+"https://www.youtube.com/watch?v=I2rmvWNwKz0",
+"https://www.youtube.com/watch?v=no3B0uS6nLk",
+"https://www.youtube.com/watch?v=3oQHMREyXSw",
+"https://www.youtube.com/watch?v=L7I9we5jP3I"
+]
