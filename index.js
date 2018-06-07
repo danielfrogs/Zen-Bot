4f34ca9102f28ce0d324de9f6c1c02311f9591e2
Daniel Behan <danielfrogs@users.noreply.github.com>
2018-06-07 11:03:23 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,66 +1,60 @@
+//Allowing access to files made within the project folder
+const botConfig = require("./botconfig.json");
+const musicPlaylist = require("./musicplaylist.js").musicPlaylist;
+
 const Discord = require("discord.js");
 const bot = new Discord.Client({autoReconnect: true, maxMessageCache: 0});
+const prefix = botConfig.prefix;
 
-const urlsPlaylist = require("./urlsArray.js").urlsPlaylist;
+const ytdl = require("ytdl-core");
+const streamOptions = {seek: 0, volume: 0.5};
 
-//The bot token goes within the quote marks
-const token = "BOT TOKEN GOES HERE";
 
-//The bot will only start reacting to information after ready is outputted
-bot.on("ready", () => {
-    console.log("Bot is active!");
+//Outputs to the console when bot is active
+bot.on("ready", async () => {
+    console.log(`Logged in as ${bot.user.username}!`);
 });
 
-//Event listener for when the message "ping" is inputted
-bot.on("message", message => {
-    if(message.content === "ping") {
-        message.channel.sendMessage("pong");
+//Command to check bot is receiving commands
+bot.on("message", async message => {
+
+    if(message.author.bot) return; //Checks to see if person who types the command is a bot; if true stops the function
+    if(message.channel.type === "dm") return; //Checks to see if the command was typed in dm; if true stops the function
+
+    if(message.content === `${prefix}ping`) {
+        message.channel.send("pong");
     }
+    else return; //If all conditions in the if statement are not met function stops
 });
 
-//Event listener for when both "joinvoice" and "leavevoice" are inputted
-bot.on("message", message => {
-    //Voice channel ID that the bot will connect to when the command "joinvoice" is inputted goes within the quote marks
-    let voiceChannel = bot.channels.get("296737276405350402");
-    if(message.content === "play track") {
-        voiceChannel.join()
-        .then(connection => {
-            //Audio file directory, in this case the root directory goes within the quote marks
-            return connection.playFile("./song.wav");
-        })
-        .then(dispatcher => {
-            dispatcher.on("error", console.error);
-        })
-        .catch(console.error);
-    }
-});
+//Command to start music looping in a desired voice channel
+bot.on("message", async message => {
 
-//Event listener for when both "joinvoice" and "leavevoice" are inputted
-bot.on("message", message => {
-    //Voice channel ID that the bot will connect to when the command "joinvoice" is inputted goes within the quote marks
-    let voiceChannel = bot.channels.get("VOICE CHANNEL ID GOES HERE");
-    const ytdl = require("ytdl-core");
-    const streamOptions = {seek: 0, volume: 0.5};
-    if(message.content === "play youtube") {
-        function looping() {
-            //Random number representing position within the urlsPlaylist array to select url. End number must be equal to the total amount of possibly selected urls from the array discounting position 0
-            randomPosition = Math.round(Math.random() * 4); 
-            console.log("Song position in array:", randomPosition);
+    let voiceChannel = bot.channels.get("VOICE CHANNEL ID GOES HERE"); //Specifies the voice channel id (optained from discord)
 
-            //Bot joins voice channel and plays the YouTube stream collected from urlsPlaylist array
-            voiceChannel.join()
-            .then(connection => {
-                let stream = ytdl(urlsPlaylist[randomPosition], {filter: "audioonly", quality: "lowest"});
-                const dispatcher = connection.playStream(stream, streamOptions);
+    if(message.content === `${prefix}youtube`) {
+        function loopNumber() { //A looping function called loopNumber
+
+            var randomPosition = musicPlaylist[Math.floor(Math.random()*musicPlaylist.length)]; //Selects a random place within the musicPlaylist array in the musicplaylist.js file
+            console.log(randomPosition);
+
+            //Bot joins the specified voice channel
+            voiceChannel.join().then(connection => {
+                
+                //Bot starts to stream using ytdl the randomPosition variable we generated above using our array in the musicplaylist.js file
+                let stream = ytdl(randomPosition);
+                const dispatcher = connection.playStream(stream, streamOptions); //streamOptions are specified at a const at the top of the index.js file
                 dispatcher.on("end", () => {
-                    looping();
+              
+                    loopNumber(); //Calls to do the function loopNumber again
+
                 });
-            })  
+            })
             .catch(console.error);
         }
-        looping();
+    loopNumber(); //Calls to do the function loopNumber again  
     }
 });
 
-//Log the Discord bot application in
-bot.login(token);
+//Login the bot with the token specified in the botConfig.json file
+bot.login(botConfig.token);
